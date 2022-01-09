47e58557dde661a60fe9ccabbc0746104e9aa702
Samuel Ebuka <samuel.ebuka@gmail.com>
2022-01-09 10:05:08 +0100

    Initial Commit

---
+++ Project8/Project8App.swift
@@ -0,0 +1,17 @@
+//
+//  Project8App.swift
+//  Project8
+//
+//  Created by Samuel Ebuka on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Project8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
